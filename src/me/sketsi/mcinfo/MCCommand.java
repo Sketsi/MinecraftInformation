2cb83ccb506431537f4ed4460b2ee17a88e6ec07
Sketsi <31267743+Sketsi@users.noreply.github.com>
2018-11-16 18:42:39 -0600

    Update MCCommand.java
    Not done, just a checkpoint..

--- src/me/sketsi/mcinfo/MCCommand.java
+++ src/me/sketsi/mcinfo/MCCommand.java
@@ -15,8 +15,9 @@ final class CommandType {
 	static final String HELP = "help";
 	static final String INFO = "info";
 	static final String ENCHANT = "enchant";
+	static final String POTION = "potion";
 }
-
+//	Enchant
 class VanillaEnchant {
 	private String name;
 	private String description;
@@ -68,6 +69,65 @@ class VanillaEnchantGroup {
 		return enchants;
 	}
 }
+//	Potion
+class VanillaPotion{
+	private String name;
+	private String description;
+	private boolean enhance;
+	private boolean extend;
+	private boolean isNegative;
+	private boolean isMixed;
+	
+	public VanillaPotion(String name, String description, boolean enhance, boolean extend) {
+		this.name = name;
+		this.description = description;
+		this.enhance = enhance;
+		this.extend = extend;
+		this.isNegative = false;
+		this.isMixed = false;
+	}
+	
+	public VanillaPotion(String name, String description, boolean enhance, boolean extend, boolean isNegative, boolean isMixed) {
+		this.name = name;
+		this.description = description;
+		this.enhance = enhance;
+		this.extend = extend;
+		this.isNegative = isNegative;
+		this.isMixed = isMixed;
+	}
+
+	String getName() {
+		return name;
+	}
+
+	public Object getFormattedName() {
+		return ChatColor.translateAlternateColorCodes('&', (this.isNegative ? "&6" : "&a") + this.name);
+	}
+	
+	public String toString() {
+		return ChatColor.translateAlternateColorCodes('&', String.format("%s&l%s &b: &3%s %s %s",
+				(this.isNegative ? "&c" : "&a"), (this.isMixed ? "&6" : ""), this.name, this.description, (this.enhance ? "" : "&aEnhanceable"), (this.extend ? "" : "&aExtendable")));
+	}
+	
+}
+
+class VanillaPotionGroup {
+	private String name;
+	private List<String> potions;
+
+	VanillaPotionGroup(String name, List<String> potions) {
+		this.name = name;
+		this.potions = potions;
+	}
+
+	String getName() {
+		return name;
+	}
+
+	List<String> getPotions() {
+		return potions;
+	}
+}
 
 public class MCCommand implements CommandExecutor {
 	private static void sendMessage(CommandSender sender, String message) {
@@ -76,7 +136,7 @@ public class MCCommand implements CommandExecutor {
 		} catch (Exception ignored) {
 		}
 	}
-
+// Enchants
 	private static Map<String, VanillaEnchant> createEnchants() {
 		Map<String, VanillaEnchant> internalMap = new HashMap<>();
 		List<VanillaEnchant> enchantList = Stream.of(
@@ -127,18 +187,21 @@ public class MCCommand implements CommandExecutor {
 
 	private static Map<String, VanillaEnchantGroup> createEnchantMap() {
 		Map<String, VanillaEnchantGroup> enchantMap = new HashMap<>();
-
+		// /mc enchants list
 		enchantMap.put("armor", new VanillaEnchantGroup("Armor", Stream.of("aquaaffinity", "blastprotection", "curseofbinding", "depthstrider", "featherfalling", "fireprotection", "frostwalker", "respiration", "projectileprotection", "protection", "thorns").collect(Collectors.toList())));
-		enchantMap.put("bow", new VanillaEnchantGroup("Bow", Stream.of("flame", "infinity", "multishot", "piercing", "punch", "power", "quickcharge").collect(Collectors.toList())));
+		enchantMap.put("bows", new VanillaEnchantGroup("Bows", Stream.of("flame", "infinity", "multishot", "piercing", "punch", "power", "quickcharge").collect(Collectors.toList())));
 		enchantMap.put("other", new VanillaEnchantGroup("Other Enchantments", Stream.of("curseofvanishing", "mending", "unbreaking").collect(Collectors.toList())));
 		enchantMap.put("tools", new VanillaEnchantGroup("Tools", Stream.of("channeling", "efficiency", "fortune", "loyalty", "luckofthesea", "lure", "riptide", "silktouch").collect(Collectors.toList())));
-		enchantMap.put("weapons", new VanillaEnchantGroup("Weapons", Stream.of("baneofarthropods", "fireaspect", "knockback", "looting", "sharpness", "smite", "sweepingedge").collect(Collectors.toList())));
-		enchantMap.put("trident", new VanillaEnchantGroup("Trident", Stream.of("channeling", "curseofvanishing", "loyalty", "riptide", "unbreaking").collect(Collectors.toList())));
+		enchantMap.put("weapons", new VanillaEnchantGroup("Weapons", Stream.of("baneofarthropods", "fireaspect", "impaling",  "knockback", "looting", "sharpness", "smite", "sweepingedge").collect(Collectors.toList())));
+		// /mc enchants list <item>
 		enchantMap.put("helmet", new VanillaEnchantGroup("Helmet", Stream.of("aquaaffinity", "blastprotection", "curseofbinding", "curseofvanishing", "fireprotection", "mending", "projectileprotection", "protection", "respiration", "thorns", "unbreaking").collect(Collectors.toList())));
 		enchantMap.put("turtleshell", new VanillaEnchantGroup("Turtle Shell", Stream.of("aquaaffinity", "blastprotection", "curseofbinding", "curseofvanishing", "fireprotection", "mending", "projectileprotection", "protection", "respiration", "thorns", "unbreaking").collect(Collectors.toList())));
 		enchantMap.put("chestplate", new VanillaEnchantGroup("Chestplate", Stream.of("blastprotection", "curseofbinding", "curseofvanishing", "fireprotection", "mending", "projectileprotection", "protection", "thorns", "unbreaking").collect(Collectors.toList())));
 		enchantMap.put("leggings", new VanillaEnchantGroup("Leggings", Stream.of("blastprotection", "curseofbinding", "curseofvanishing", "fireprotection", "mending", "projectileprotection", "protection", "thorns", "unbreaking").collect(Collectors.toList())));
 		enchantMap.put("boots", new VanillaEnchantGroup("Boots", Stream.of("blastprotection", "curseofbinding", "curseofvanishing", "fireprotection", "mending", "projectileprotection", "protection", "thorns", "unbreaking").collect(Collectors.toList())));
+		enchantMap.put("bow", new VanillaEnchantGroup("Bow", Stream.of("curseofvanishing", "flame", "infinity", "mending", "punch", "power").collect(Collectors.toList())));
+		enchantMap.put("crossbow", new VanillaEnchantGroup("Crossbow", Stream.of("multishot", "piercing", "quickcharge").collect(Collectors.toList())));
+		enchantMap.put("trident", new VanillaEnchantGroup("Trident", Stream.of("channeling", "curseofvanishing", "impaling", "loyalty", "riptide", "unbreaking").collect(Collectors.toList())));
 		enchantMap.put("sword", new VanillaEnchantGroup("Sword", Stream.of("baneofarthropods", "curseofvanishing", "fireaspect", "knockback", "looting", "mending", "sharpness", "smite", "sweepingedge", "unbreaking").collect(Collectors.toList())));
 		enchantMap.put("pickaxe", new VanillaEnchantGroup("Pickaxe", Stream.of("curseofvanishing", "efficiency", "fortune", "mending", "silktouch", "unbreaking").collect(Collectors.toList())));
 		enchantMap.put("shovel", new VanillaEnchantGroup("Shovel", Stream.of("curseofvanishing", "efficiency", "fortune", "mending", "silktouch", "unbreaking").collect(Collectors.toList())));
@@ -163,7 +226,7 @@ public class MCCommand implements CommandExecutor {
 
 	private final Map<String, VanillaEnchantGroup> enchantMap = createEnchantMap();
 
-	private final String[] defaultGroups = new String[]{"armor", "bow", "other", "tools", "weapons"};
+	private final String[] defaultEnchantGroups = new String[]{"armor", "bows", "other", "tools", "weapons"};
 
 	private boolean sendEnchantList(CommandSender sender, String group) {
 		VanillaEnchantGroup enchantgroup = enchantMap.get(group);
@@ -174,13 +237,73 @@ public class MCCommand implements CommandExecutor {
 
 		return true;
 	}
+//	Potion
+	private Map<String, VanillaPotion> createPotions() {
+		Map<String, VanillaPotion> internalMap = new HashMap<>();
+		List<VanillaPotion> potionList = Stream.of(
+				//	Base
+				new VanillaPotion("Awkward", "Water Bottle + Nether Wart", false, false),
+				new VanillaPotion("Mundane", "Water Bottle + (Blaze Powder, Ghast Tear, Glistering Melon, Golden Carrot, Magma Cream, Phantom Membrane, Pufferfish, Rabbit's Foot, Redstone, Spider Eye,Sugar or Turtle Shell)", false, false),
+				new VanillaPotion("Thick", "Water Bottle + Glowstone", false, false),
+				//	Positive
+				new VanillaPotion("FireResistance", "Awkward Potion + Magma Cream", false, true),
+				new VanillaPotion("Healing", "Awkward Potion + Gliering Melon", true, false),
+				new VanillaPotion("Invisibility", "NightVision Potion + Fermented Spider Eye", false, true),
+				new VanillaPotion("Leaping", "Awkward Potion + Rabbit's Foot", true, true),
+				new VanillaPotion("NightVision", "Awkward Potion + Golden Carrot", false, true),
+				new VanillaPotion("Regeneration", "Awkward Potion + Ghast Tear", true, true),
+				new VanillaPotion("SlowFalling", "Awkward Potion + Phantom Membrane", false, true),
+				new VanillaPotion("Strength", "Awkward Potion + Blaze Powder", true, true),
+				new VanillaPotion("Swiftness", "Awkward Potion + Sugar", true, true),
+				new VanillaPotion("WaterBreathing", "Awkward Potion + Pufferfish", false, true),
+				//	Mixed
+				new VanillaPotion("TurtleMaster", "Awkward Potion + Turtle Shell", true, true, false, true),
+				//	Negative
+				new VanillaPotion("Harming", "Health Potion + Fermented Spider Eye", true, false, true, false),
+				new VanillaPotion("Poison", "Awkward Potion + Spider Eye", false, true, true, false),
+				new VanillaPotion("Slowness", "Swiftness + Fermented Spider Eye", true, true, true, false),
+				new VanillaPotion("Weakness", "Awkward/Strength Potion + Fermented Spider Eye", true, false, true, false)
+				).collect(Collectors.toList());
 
+		for (VanillaPotion potion : potionList)
+			internalMap.put(potion.getName().toLowerCase(), potion);
+
+		return internalMap;
+	}
+	
+	private static Map<String, VanillaPotionGroup> createPotionMap() {
+		Map<String, VanillaPotionGroup> potionMap = new HashMap<>();
+		potionMap.put("base", new VanillaPotionGroup("Base", Stream.of("Awkward", "Mundane", "Thick").collect(Collectors.toList())));
+		potionMap.put("positive", new VanillaPotionGroup("Positive", Stream.of("FireResistance", "Healing", "Leaping", "NightVision", "SlowFalling", "Strength", "Swiftness", "WaterBreathing").collect(Collectors.toList())));
+		potionMap.put("mixed", new VanillaPotionGroup("Mixed", Stream.of("TurtleMaster").collect(Collectors.toList())));
+		potionMap.put("negative", new VanillaPotionGroup("Negative", Stream.of("Harming", "Poison", "Slowness", "Weakness").collect(Collectors.toList())));
+		
+		return potionMap;
+	}
+	
+	private final Map<String, VanillaPotion> potions = createPotions();
+	
+	private final Map<String,VanillaPotionGroup> potionMap = createPotionMap();
+	
+	private final String[] defaultPotionGroups = new String[]{"base", "positive", "mixed", "negative"};
+	
+	private boolean sendPotionList(CommandSender sender, String group) {
+		VanillaPotionGroup potiongroup = potionMap.get(group);
+
+		sendMessage(sender, String.format("&b&l%s&b:", potionMap.get(group).getName()));
+		sendMessage(sender, String.format("&7[%s&7]", potiongroup.getPotions().stream().parallel()
+				.map(key -> potions.get(key).getFormattedName()).collect(Collectors.joining("&7, "))));
+
+		return true;
+	}
+	
 	@SuppressWarnings("unused")
 	private boolean sendDefault(CommandSender sender) {
 		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6MCInfo: &cUse \"&4&o/mc help&c\"!"));
 		return true;
 	}
 
+	
 	private boolean sendHelp(CommandSender sender, String command) {
 		if (command == null || command.isEmpty()) {
 			return false;
@@ -193,12 +316,16 @@ public class MCCommand implements CommandExecutor {
 			case CommandType.ENCHANT:
 				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6MCInfo: &cUse \"&4&o/mc enchant list [item]&c\" for available options!"));
 				break;
-			case CommandType.HELP: // Falls through
+			case CommandType.POTION:
+				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6MCInfo: &cUse \"&4&o/mc potion list&c\" for more help!"));	
+			case CommandType.HELP: // Falls through	
 			default:
 				sender.sendMessage(ChatColor.GOLD + "Minecraft Information Help:");
-				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mc info <enchantment> &8- &7Get information on an enchantment."));
 				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mc help &8- &7Brings this list up."));
+				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mc info <enchantment> &8- &7Get information on an enchantment."));
 				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mc enchant list [item] &8- &7Lists all enchantments, if you specify item, it'll show specifics."));
+//				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mc potion brew [effect] &8- &7Lists the ingredients for the potion effect."));
+				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mc potion list &8- &7Lists all vanilla effects."));
 				return true;
 		}
 
@@ -227,13 +354,13 @@ public class MCCommand implements CommandExecutor {
 
 	private boolean handleEnchantCommand(CommandSender sender, String[] args) {
 		// User didn't enter enough arguments to
-		if (args.length < 2 || !args[1].equals("list")) {
+		if (args.length < 2 || !args[1].equals("list") || !args[1].equals("info")) {
 			return sendHelp(sender, CommandType.ENCHANT);
 		}
 
 		if (args.length == 2) {
 			sender.sendMessage(ChatColor.AQUA + "=========================================");
-			for (String group : defaultGroups) sendEnchantList(sender, group);
+			for (String group : defaultEnchantGroups) sendEnchantList(sender, group);
 			sender.sendMessage(ChatColor.AQUA + "=========================================");
 			return true;
 		}
@@ -242,6 +369,25 @@ public class MCCommand implements CommandExecutor {
 			return sendHelp(sender, CommandType.ENCHANT);
 
 		return sendEnchantList(sender, args[2]);
+	}
+	
+	private boolean handlePotionCommand(CommandSender sender, String[] args) {
+		// User didn't enter enough arguments to
+		if (args.length < 2 || !args[1].equals("list")) {
+			return sendHelp(sender, CommandType.POTION);
+		}
+		
+		if (args.length == 2) {
+			sender.sendMessage(ChatColor.AQUA + "=========================================");
+			for (String group : defaultPotionGroups) sendPotionList(sender, group);
+			sender.sendMessage(ChatColor.AQUA + "=========================================");
+			return true;
+		}
+		
+		if (!potionMap.containsKey(args[2]))
+			return sendHelp(sender, CommandType.POTION);
+
+		return sendPotionList(sender, args[2]);
 	}
 
 	@Override
@@ -258,10 +404,14 @@ public class MCCommand implements CommandExecutor {
 				return handleInfoCommand(sender, args);
 			case CommandType.ENCHANT:
 				return handleEnchantCommand(sender, args);
+			case CommandType.POTION:
+				return handlePotionCommand(sender, args);
 			default:
 // 				return sendDefault(sender);
 				return sendHelp(sender);
 		}
 	}
+
+	
 
 }
