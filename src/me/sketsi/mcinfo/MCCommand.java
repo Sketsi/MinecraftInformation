28e1bb802b058f38e79d1fc2998f1390c667b64e
Sketsi <31267743+Sketsi@users.noreply.github.com>
2018-11-18 15:50:55 -0600

    Update MCCommand.java
    Added Potions to the mix.

--- src/me/sketsi/mcinfo/MCCommand.java
+++ src/me/sketsi/mcinfo/MCCommand.java
@@ -12,10 +12,13 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 final class CommandType {
-	static final String HELP = "help";
-	static final String INFO = "info";
+	// First Arg
 	static final String ENCHANT = "enchant";
 	static final String POTION = "potion";
+	// Second Arg
+	static final String HELP = "help";
+	static final String INFO = "info";
+	static final String LIST = "list";
 }
 //	Enchant
 class VanillaEnchant {
@@ -101,7 +104,7 @@ class VanillaPotion{
 	}
 
 	public Object getFormattedName() {
-		return ChatColor.translateAlternateColorCodes('&', (this.isNegative ? "&6" : "&a") + this.name);
+		return ChatColor.translateAlternateColorCodes('&', (this.isNegative ? "&c" : "&a") + (this.isMixed ? "&6" : "") + this.name);
 	}
 	
 	public String toString() {
@@ -324,7 +327,7 @@ public class MCCommand implements CommandExecutor {
 				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mc help &8- &7Brings this list up."));
 				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mc info <enchantment> &8- &7Get information on an enchantment."));
 				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mc enchant list [item] &8- &7Lists all enchantments, if you specify item, it'll show specifics."));
-//				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mc potion brew [effect] &8- &7Lists the ingredients for the potion effect."));
+				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mc potion info <effect> &8- &7Lists the ingredients for the potion effect."));
 				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c/mc potion list &8- &7Lists all vanilla effects."));
 				return true;
 		}
@@ -335,8 +338,25 @@ public class MCCommand implements CommandExecutor {
 	private boolean sendHelp(CommandSender sender) {
 		return sendHelp(sender, CommandType.HELP);
 	}
+	
+//	Enchant
+	public boolean onEnchantCommand(CommandSender sender, String[] args) {
+        if (args.length < 2) {
+            // Send help to the user when the command is executed with no arguments -- can be replaced with any function call.
+            return sendHelp(sender, CommandType.ENCHANT);
+        }
 
-	private boolean handleInfoCommand(CommandSender sender, String[] args) {
+        switch (args[1].toLowerCase()) {
+            case CommandType.INFO:
+                return handleEnchantInfoCommand(sender, args);
+            case CommandType.LIST:
+                return handleEnchantListCommand(sender, args);
+            default:
+                return sendHelp(sender, CommandType.ENCHANT);
+        }
+    }
+	
+	private boolean handleEnchantInfoCommand(CommandSender sender, String[] args) {
 		// User didn't enter enough arguments to send info for a particular enchantment
 		if (args.length < 2) {
 			return sendHelp(sender, CommandType.INFO);
@@ -352,9 +372,9 @@ public class MCCommand implements CommandExecutor {
 		return true;
 	}
 
-	private boolean handleEnchantCommand(CommandSender sender, String[] args) {
+	private boolean handleEnchantListCommand(CommandSender sender, String[] args) {
 		// User didn't enter enough arguments to
-		if (args.length < 2 || !args[1].equals("list") || !args[1].equals("info")) {
+		if (args.length < 2 || !args[1].equals("list")) {
 			return sendHelp(sender, CommandType.ENCHANT);
 		}
 
@@ -370,26 +390,59 @@ public class MCCommand implements CommandExecutor {
 
 		return sendEnchantList(sender, args[2]);
 	}
-	
-	private boolean handlePotionCommand(CommandSender sender, String[] args) {
+
+// Potion	
+	public boolean onPotionCommand(CommandSender sender, String[] args) {
+        if (args.length < 2) {
+            // Send help to the user when the command is executed with no arguments -- can be replaced with any function call.
+            return sendHelp(sender, CommandType.POTION);
+        }
+
+        switch (args[1].toLowerCase()) {
+            case CommandType.INFO:
+                return handlePotionInfoCommand(sender, args);
+            case CommandType.LIST:
+                return handlePotionListCommand(sender, args);
+            default:
+                return sendHelp(sender, CommandType.POTION);
+        }
+    }
+
+	private boolean handlePotionInfoCommand(CommandSender sender, String[] args) {
+		// User didn't enter enough arguments to send info for a particular enchantment
+		if (args.length < 2) {
+			return sendHelp(sender, CommandType.INFO);
+		}
+
+		// User entered an enchantment that doesn't exist in our list
+		if (!potions.containsKey(args[1].toLowerCase())) {
+			return sendHelp(sender, CommandType.INFO);
+		}
+
+		sender.sendMessage(potions.get(args[1].toLowerCase()).toString());
+
+		return true;
+	}
+
+	private boolean handlePotionListCommand(CommandSender sender, String[] args) {
 		// User didn't enter enough arguments to
 		if (args.length < 2 || !args[1].equals("list")) {
 			return sendHelp(sender, CommandType.POTION);
 		}
-		
+
 		if (args.length == 2) {
 			sender.sendMessage(ChatColor.AQUA + "=========================================");
 			for (String group : defaultPotionGroups) sendPotionList(sender, group);
 			sender.sendMessage(ChatColor.AQUA + "=========================================");
 			return true;
 		}
-		
+
 		if (!potionMap.containsKey(args[2]))
 			return sendHelp(sender, CommandType.POTION);
 
 		return sendPotionList(sender, args[2]);
 	}
-
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (args.length < 1) {
@@ -400,18 +453,15 @@ public class MCCommand implements CommandExecutor {
 		switch (args[0].toLowerCase()) {
 			case CommandType.HELP:
 				return sendHelp(sender);
-			case CommandType.INFO:
-				return handleInfoCommand(sender, args);
 			case CommandType.ENCHANT:
-				return handleEnchantCommand(sender, args);
+				return onEnchantCommand(sender, args);
 			case CommandType.POTION:
-				return handlePotionCommand(sender, args);
+				return onPotionCommand(sender, args);
 			default:
 // 				return sendDefault(sender);
 				return sendHelp(sender);
 		}
 	}
-
 	
-
 }
+
